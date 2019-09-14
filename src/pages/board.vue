bf91d8bb80dd4771f5f62302f9caaf786332e2fe
Ilya <gerasimov_981@mail.ru>
2019-09-14 22:39:55 +0300

    [mod] right menu

--- src/pages/board.vue
+++ src/pages/board.vue
@@ -1,26 +1,25 @@
 <template>
-    <f7-page class="board-page" @click="rightMenu=!false">
+    <f7-page class="board-page">
         <div>
             <f7-navbar  class="board-page-navbar">
-                <f7-nav-left>
-                    <i class="f7-icons" style="padding-left: 10px;color: white;cursor:pointer;"  @click="back">chevron_left</i>
-                </f7-nav-left>
                 <f7-nav-title style="padding-left: 10px;color: white;">Голосования</f7-nav-title>
                 <f7-nav-right>
-                    <i class="f7-icons"
-                       style="padding-right: 10px;color: white;cursor:pointer;"
-                       @click="rightMenu=!rightMenu">bars</i>
+                    <f7-button
+                            raised panel-open="right"
+                    >
+                        <i class="f7-icons"
+                           style="color: white;cursor:pointer;">bars</i>
+                    </f7-button>
                 </f7-nav-right>
             </f7-navbar>
         </div>
-        <f7-panel right resizable theme-dark :opened="rightMenu">
+        <f7-panel right resizable>
             <f7-view>
                 <f7-page>
                     <f7-block>Right panel content</f7-block>
                 </f7-page>
             </f7-view>
         </f7-panel>
-
         <room-item-list
             :events="events"
         >
@@ -40,14 +39,11 @@
     },
     data () {
       return {
-        rightMenu: false
+
       }
     },
 
     methods: {
-      back () {
-        this.$f7router.back();
-      }
     },
 
     created () {
@@ -62,8 +58,21 @@
     @import "../css/main";
 
     .board-page {
+        background: $main-background;
+
         &-navbar {
             background: $main-color;
+
+            .button  {
+                border: none;
+                color: transparent;
+                border-radius: 0;
+                line-height: 0;
+                height: 29px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
         }
     }
 
