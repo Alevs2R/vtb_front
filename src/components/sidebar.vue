9abbc3b4d8a3ef4d18d0280c849fdba87b8de02b
alevs2r <a.evlampev@dooglys.com>
2019-09-14 22:44:20 +0300

    add sidebar

---
+++ src/components/sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="sidebar">
+        <img :src="user_icon" />
+    </div>
+</template>
+
+<script>
+  import user_icon from '../assets/user.svg';
+
+  export default {
+    name: "sidebar"
+  };
+</script>
+
+<style scoped>
+    .sidebar {
+        display: block;
+        width: 250px;
+        background: white;
+        border-radius: 5px;
+        margin-right: 30px;
+    }
+
+    @media screen and (max-width: 500px) {
+        .sidebar {
+            display: none;
+        }
+    }
+</style>
