c7eb07a76adf95ca51b6920c6d4243310a318923
alevs2r <a.evlampev@dooglys.com>
2019-09-15 06:27:55 +0300

    fix scrolling

--- src/pages/createRoom.vue
+++ src/pages/createRoom.vue
@@ -1,61 +1,63 @@
 <template>
     <f7-page class="board-page">
-        <navbar></navbar>
-        <div class="container">
-            <h1 style="font-weight: normal; margin-left: 15px">Создание голосования</h1>
-            <f7-list no-hairlines-md>
-                <f7-list-input
-                        label="Название голосования"
-                        type="text"
-                        placeholder="Введите название"
-                        clear-button
-                ></f7-list-input>
+        <div>
+            <navbar></navbar>
+            <div class="container">
+                <h1 style="font-weight: normal; margin-left: 15px">Создание голосования</h1>
+                <f7-list no-hairlines-md>
+                    <f7-list-input
+                            label="Название голосования"
+                            type="text"
+                            placeholder="Введите название"
+                            clear-button
+                    ></f7-list-input>
 
-                <f7-list-input
-                        label="Описание повестки"
-                        type="textarea"
-                        placeholder="Введите описание повестки"
-                        clear-button
-                ></f7-list-input>
-            </f7-list>
-            <div class="section-title" style="margin-top: -20px">Документы</div>
-            <div class="myrow">
-                <div class="attachment">
-                    <img :src="Attachment" />
-                    PSW_web-track.pdf
-                    <div class="attachment-cross">
-                        <img :src="Multiply" />
-                    </div>
-                </div>
-                <div class="attachment">
-                    <img :src="Attachment" />
-                    Критерии оценки.doc
-                    <div class="attachment-cross">
-                        <img :src="Multiply" />
-                    </div>
-                </div>
-                <div class="light_button" style="width: 130px" @click="">Добавить</div>
-            </div>
-            <div class="section-title">Вопросы</div>
-            <div class="votings">
-
-                <f7-row>
-                    <f7-col width="100" tablet-width="50" desktop-width="50">
-                        <div class="voting">
-                            <f7-input
-                                    type="text"
-                                    placeholder="Ваш вопрос"
-                                    clear-button
-                                    size="18"
-                            ></f7-input>
-                            <div style="font-size: 12px; margin: 20px 0 10px">Варианты ответа</div>
-                            <CreateVoteOption />
-                            <CreateVoteOption />
-                            <CreateVoteOption />
-                            <div class="light_button" style="width: 250px" @click="">Добавить вариант ответа</div>
+                    <f7-list-input
+                            label="Описание повестки"
+                            type="textarea"
+                            placeholder="Введите описание повестки"
+                            clear-button
+                    ></f7-list-input>
+                </f7-list>
+                <div class="section-title" style="margin-top: -20px">Документы</div>
+                <div class="myrow">
+                    <div class="attachment">
+                        <img :src="Attachment" />
+                        PSW_web-track.pdf
+                        <div class="attachment-cross">
+                            <img :src="Multiply" />
                         </div>
-                    </f7-col>
-                </f7-row>
+                    </div>
+                    <div class="attachment">
+                        <img :src="Attachment" />
+                        Критерии оценки.doc
+                        <div class="attachment-cross">
+                            <img :src="Multiply" />
+                        </div>
+                    </div>
+                    <div class="light_button" style="width: 130px" @click="">Добавить</div>
+                </div>
+                <div class="section-title">Вопросы</div>
+                <div class="votings">
+
+                    <f7-row>
+                        <f7-col width="100" tablet-width="50" desktop-width="50">
+                            <div class="voting">
+                                <f7-input
+                                        type="text"
+                                        placeholder="Ваш вопрос"
+                                        clear-button
+                                        size="18"
+                                ></f7-input>
+                                <div style="font-size: 12px; margin: 20px 0 10px">Варианты ответа</div>
+                                <CreateVoteOption />
+                                <CreateVoteOption />
+                                <CreateVoteOption />
+                                <div class="light_button" style="width: 250px" @click="">Добавить вариант ответа</div>
+                            </div>
+                        </f7-col>
+                    </f7-row>
+                </div>
             </div>
         </div>
     </f7-page>
